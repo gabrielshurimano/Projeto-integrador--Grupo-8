35b658c76ed3fe835393c4080d0ac629b0d27876
Marcelo Francisco Regis <marcelofregis@gmail.com>
2023-01-31 19:03:56 -0300

    Atualizada Validações no fomulário

--- tele_cadastro/validacao.js
+++ tele_cadastro/validacao.js
@@ -4,11 +4,6 @@ const spans  = document.querySelectorAll(".span-required")
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 
-// Não está funcionado o Submit
-form.addEventListener('submit', (event) => {
-     console.log('teste')
-})
-
 function SetError(index){
     campos[index].style.border = "3px solid red"
     spans[index].style.display = "block";
@@ -19,43 +14,70 @@ function removeError(index){
     spans[index].style.display = "none";
 }
 
+let nomeValidado = false
 
 function nameValidate(){
     if (campos[0].value.length < 3)  {
         SetError(0);
+        nomeValidado = false;
     } else {
         removeError(0)
+        nomeValidado = true;
+          
    }
 }
 
+let emailValidado = false
 function emailValidate(){
     if (!emailRegex.test(campos[1].value)){
         SetError(1);
+        emailValidado = false
     } else {
         removeError(1)
+        emailValidado = true
 
     }
 }
+
+let senhaValidada = false
 
 function mainPasswordValidate(){
     if (campos[3].value.length < 8) {
         SetError(3);
+        senhaValidada = false
     } else {
         removeError(3);
         ComparePassword();
+        senhaValidada = true
     }
 }
+
+senhaIgual = false
 
 function ComparePassword(){
 
         if(campos[3].value == campos[4].value && campos[4].value.length >= 8){
             removeError(4);
+            senhaIgual = true
         } else {
             SetError(4);
+            senhaIgual = false
        }
 
 }
 
+form.addEventListener("submit", (event) => {
+    if (nomeValidado && emailValidado && senhaValidada && senhaIgual) { 
+     alert("Formulário enviado com sucesso")
+    } else{
+    event.preventDefault()
+    alert("Preencha todos os campos corretamente")
+    nameValidate()
+    emailValidate()
+    mainPasswordValidate()
+    ComparePassword()
+    }
+ })
 
-
+ 
 
