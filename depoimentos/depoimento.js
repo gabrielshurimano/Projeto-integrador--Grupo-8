79f4bfeb20e188fbe698fda8a575ad1ae372c809
Elton <elton-diego@live.com>
2023-02-01 15:15:11 -0300

    criação do js e modificação do htmal e css

---
+++ depoimentos/depoimento.js
@@ -0,0 +1,62 @@
+let card1 = document.getElementById("card1")
+let card2 = document.getElementById("card2")
+let card3 = document.getElementById("card3")
+let botao = document.getElementById("btn")
+
+
+
+// manipulação do card 1
+
+function cardfundo(){
+document.getElementById("card1").style.backgroundColor = "#6D6A75"
+
+
+
+}   
+
+
+function cardfundo1(){
+    document.getElementById("card1").style.backgroundColor = "#A299AD"
+    
+
+}
+
+
+
+//manipulação card 2
+
+function cardfundo2(){
+    document.getElementById("card2").style.backgroundColor = "#6D6A75"
+}
+
+function cardfundo21(){
+
+    document.getElementById("card2").style.backgroundColor = "#A299AD"
+
+}
+
+
+
+//manipulação card 3
+
+function cardfundo3(){
+    document.getElementById("card3").style.backgroundColor = "#6D6A75"
+
+}
+
+function cardfundo22(){
+    document.getElementById("card3").style.backgroundColor = "#A299AD"
+
+}
+
+
+//manipulação do btn
+
+function btn1(){
+    document.getElementById("btn").style.backgroundColor = "green"
+
+}
+
+function btn2(){
+    document.getElementById("btn").style.backgroundColor = "#A299AD"
+}
